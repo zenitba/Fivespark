a621d65544f4232d244e2824caa03718a0780f5a
zenitba <112856019+zenitba@users.noreply.github.com>
2023-06-16 09:59:25 +0200

    usertest

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,17 +4,20 @@ window.addEventListener("load", function(){
   loader.style.display = "none";
 })
 
-// slider show voor de blog
-var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-  });
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
--- server.js
+++ server.js
@@ -1,134 +1,30 @@
-import express from 'express'
+import express from "express";
+import { resolve } from "path";
+import fetch from "node-fetch";
 
-// de basis url van de api
-const url = 'https://api.fivespark.com/admin/content'
+const server = express();
+const url = 'https://api.fivespark.com/docs/?access_token=-tfhE-ZUueSF8jxhSMgpTNr2k2yum-8f#';
 
-// maak een nieuwe express app
-const server = express()
+server.set("view engine", "ejs");
+server.set("views", "./views");
+server.set("port", process.env.PORT || 8000);
 
-// Stel het poortnummer in
-server.set('port', process.env.PORT || 8000)
+server.use(express.static(resolve('public')));
 
-// Stel de view engine in
-server.set('view engine', 'ejs')
-server.set('views', './views')
+server.listen(server.get("port"), () => {
+  console.log(`Application started on http://localhost:${server.get("port")}`);
+});
+server.get('/', (_request, response) => {
+  let itemUrl = url + '/ItemsActivities/readItemsActivities';
 
-// Stel de public map in
-server.use(express.static('public'))
-
-// Maak een route voor de index /categories
-server.get('/', (request, response) => {
- 
-    const categoriesUrl= url + `/categories`
-
-    fetchJson(categoriesUrl).then((data) => {
-      response.render('index', data)
-    })
-  })
-
-  //  pinda-ei pagina producten
-server.get("/pinda-ei", async (request, response) => {
-  const query = request.query.categorieId;
-
-  const productenUrl = url + `/producten?categorieId=${query}`;
-
-  await fetchJson(productenUrl).then((data) => {
-      response.render("pinda-ei", { data: data });
+  dataFetch(itemUrl).then((data) => {
+    response.render('index', { data: data });
   });
 });
 
-// Route voor de producten pagina
-server.get("/product-pagina", async (request, response) => {
-  const query = request.query.id;
-
-  const productenUrl = url + `/product?id=${query}`;
-
-  await fetchJson(productenUrl).then((data) => {
-      response.render("product-pagina", { data: data });
-  });
-});
-
-// Route voor de checkout pagina
-server.get("/checkout", async (request, response) => {
-  const query = request.query.id;
-
-  const productenUrl = url + `/product?id=${query}`;
-
-  await fetchJson(productenUrl).then((data) => {
-      response.render("checkout", { data: data });
-  });
-});
-
-
-
-    // definieer de fetchJson functie
-    async function fetchJson(url) {
-        return await fetch(url)
-          .then((response) => response.json())
-          .catch((error) => error)
-      }
-      
-      // Start met luisteren
-    server.listen(server.get('port'), () => {
-        console.log(`Application started on http://localhost:${server.get('port')}`)
-      })
-// import express from 'express';
-// import fetch from 'node-fetch';
-
-// const url = 'https://api.fivespark.com/admin/content';
-
-// const server = express();
-// server.set('port', process.env.PORT || 8000);
-// server.set('view engine', 'ejs');
-// server.set('views', './views');
-// server.use(express.static('public'));
-
-// server.get('/', (request, response) => {
-//   const pillarsUrl = url + '/pillars';
-
-//   fetchJson(pillarsUrl)
-//     .then((data) => {
-//       response.render('index', data);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       response.sendStatus(500);
-//     });
-// });
-
-// server.get("/pinda-ei", async (request, response) => {
-//   const query = request.query.pillarsId;
-
-//   const activitiesUrl = url + `/activities?pillarsId=${query}`;
-
-//   await fetchJson(activitiesUrl)
-//     .then((data) => {
-//       response.render("pinda-ei", { data: data });
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       response.sendStatus(500);
-//     });
-// });
-// async function fetchJson(url) {
-//   try {
-//     const response = await fetch(url);
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-
-//     const contentType = response.headers.get('content-type');
-//     if (!contentType || !contentType.includes('application/json')) {
-//       throw new Error('Response is not in JSON format!');
-//     }
-
-//     return await response.json();
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-//       // Start met luisteren
-//       server.listen(server.get('port'), () => {
-//         console.log(`Application started on http://localhost:${server.get('port')}`)
-//       })
+async function dataFetch(url) {
+  const data = await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+  return data;
+}
