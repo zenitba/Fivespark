f1ea1c4e598916d876aa4287eecb33a0b16a3b54
zenitba <112856019+zenitba@users.noreply.github.com>
2023-06-21 12:20:11 +0200

    Update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,57 @@
 // loader
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-  loader.style.display = "none";
-})
 
+if (typeof window === 'undefined') {
+  // JavaScript is disabled
+  loader.classList.add("js-disable");
+} else {
+  // JavaScript is enabled
+  loader.classList.add("js-enable");
+
+  window.addEventListener("load", function(){
+    loader.style.display = "none";
+  });
+}
+
+// Hamburger menu PE
 const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
-  navbar.classList.add("show");
-  menuBtn.classList.add("hide");
-  body.classList.add("disabled");
+
+// Add .js-enable class to body
+body.classList.add("js-enable");
+
+if (menuBtn && cancelBtn) {
+  menuBtn.onclick = () => {
+    navbar.classList.add("show");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabled");
+  };
+
+  cancelBtn.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+  };
 }
-cancelBtn.onclick = ()=>{
-  body.classList.remove("disabled");
-  navbar.classList.remove("show");
-  menuBtn.classList.remove("hide");
-}
-window.onscroll = ()=>{
+
+window.onscroll = () => {
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+};
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    } 
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
--- server.js
+++ server.js
@@ -1,30 +1,55 @@
-import express from "express";
-import { resolve } from "path";
+import express from "express"
+import bodyParser from "body-parser"
+import * as path from "path"
+import * as dotenv from "dotenv"
 import fetch from "node-fetch";
 
-const server = express();
-const url = 'https://api.fivespark.com/docs/?access_token=-tfhE-ZUueSF8jxhSMgpTNr2k2yum-8f#';
+// load .env file
+dotenv.config()
 
-server.set("view engine", "ejs");
-server.set("views", "./views");
-server.set("port", process.env.PORT || 8000);
+const server = express()
 
-server.use(express.static(resolve('public')));
+server.set("view engine", "ejs")
+server.set("views", "./views")
+server.set("port", process.env.PORT || 8000)
+
+server.use(express.static(path.resolve('public')))
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}))
 
 server.listen(server.get("port"), () => {
-  console.log(`Application started on http://localhost:${server.get("port")}`);
+	console.log(`Application started on http://localhost:${server.get("port")}`)
 });
-server.get('/', (_request, response) => {
-  let itemUrl = url + '/ItemsActivities/readItemsActivities';
 
-  dataFetch(itemUrl).then((data) => {
-    response.render('index', { data: data });
-  });
-});
+
+// FUNCTIONS && VARIABLES
+
+server.get("/", async (req, res) => {
+	const serviceContracts = await dataFetch("https://api.fivespark.com/items/service_contracts")
+	const selectedContracts = serviceContracts.data.filter(contract => contract.id === 3 || contract.id === 4 || contract.id === 7)
+	const activitiesServicesContracts = await dataFetch("https://api.fivespark.com/items/activities_service_contracts")
+	
+	const selectedContractTranslations = contractTranslations.data.filter(translation => translation.id === 3 || translation.id === 4 || translation.id === 7)
+	const contract_basic = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 3)
+	const contract_complete = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 4)
+	const contract_premium = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 7)
+	
+
+	res.render("index", { selectedContracts, contract_basic, contract_premium, contract_complete, activityTranslations, language } )
+})
+
+/* ---------------------------- Api call function --------------------------- */
+
+const apiInformation = {
+	method: "GET",
+	headers: {
+		Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+	}
+}
 
 async function dataFetch(url) {
-  const data = await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error);
-  return data;
+	const data = await fetch(url, apiInformation)
+		.then((response) => response.json())
+		.catch((error) => error);
+	return data;
 }
