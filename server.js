d06370cf8ad93de70fdb3524d00cc56940307720
zenitba <112856019+zenitba@users.noreply.github.com>
2023-06-22 20:47:03 +0200

    nog steeds error

--- server.js
+++ server.js
@@ -1,47 +1,24 @@
-import express from "express"
-import bodyParser from "body-parser"
-import * as path from "path"
-import * as dotenv from "dotenv"
-import fetch from "node-fetch";
-
-// load .env file
-dotenv.config()
-
-const server = express()
-
-server.set("view engine", "ejs")
-server.set("views", "./views")
-server.set("port", process.env.PORT || 8000)
-
-server.use(express.static(path.resolve('public')))
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({extended: true}))
-
-server.listen(server.get("port"), () => {
-	console.log(`Application started on http://localhost:${server.get("port")}`)
-});
-
-
-// FUNCTIONS && VARIABLES
-
 server.get("/", async (req, res) => {
 
-	const serviceContracts = await dataFetch("https://api.fivespark.com/items/service_contracts")
-	const selectedContracts = serviceContracts.data.filter(contract => contract.id === 3 || contract.id === 4 || contract.id === 7)
-	
-
-	const activitiesServicesContracts = await dataFetch("https://api.fivespark.com/items/activities_service_contracts")
-	
-	const contract_basic = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 3)
-	const contract_complete = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 4)
-	const contract_premium = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 7)
 
 
-	const activityTranslations = await dataFetch("https://api.fivespark.com/items/activities_translations")
+  const serviceContracts = await dataFetch("https://api.fivespark.com/items/service_contracts")
+  const selectedContracts = serviceContracts.data.filter(contract => contract.id === 3 || contract.id === 4 || contract.id === 7)
 
-	console.log(activityTranslations)
 
-	res.render("index", { selectedContracts, contract_basic, contract_premium, contract_complete, activityTranslations, language })
+  const activitiesServicesContracts = await dataFetch("https://api.fivespark.com/items/activities_service_contracts")
+
+  const contract_basic = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 3)
+  const contract_complete = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 4)
+  const contract_premium = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 7)
+
+
+  const activityTranslations = await dataFetch("https://api.fivespark.com/items/activities_translations")
+
+  console.log(activityTranslations)
+  
+
+  res.render("index", { selectedContracts, contract_basic, contract_premium, contract_complete, activityTranslations,})
 })
 
 
