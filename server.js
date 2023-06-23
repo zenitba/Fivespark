92543f9ae0ee7d7643d946f778a2b9223c2c0c62
zenitba <112856019+zenitba@users.noreply.github.com>
2023-06-23 11:18:18 +0200

    error cyclic

--- server.js
+++ server.js
@@ -22,26 +22,33 @@ server.listen(server.get("port"), () => {
 });
 
 
+// FUNCTIONS && VARIABLES
+
 server.get("/", async (req, res) => {
 
 
-  const serviceContracts = await dataFetch("https://api.fivespark.com/items/service_contracts")
-  const selectedContracts = serviceContracts.data.filter(contract => contract.id === 3 || contract.id === 4 || contract.id === 7)
+
+	const serviceContracts = await dataFetch("https://api.fivespark.com/items/service_contracts")
+	const selectedContracts = serviceContracts.data.filter(contract => contract.id === 3 || contract.id === 4 || contract.id === 7)
 
 
-  const activitiesServicesContracts = await dataFetch("https://api.fivespark.com/items/activities_service_contracts")
+	const activitiesServicesContracts = await dataFetch("https://api.fivespark.com/items/activities_service_contracts")
 
-  const contract_basic = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 3)
-  const contract_complete = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 4)
-  const contract_premium = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 7)
+	const contract_basic = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 3)
+	const contract_complete = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 4)
+	const contract_premium = activitiesServicesContracts.data.filter(contract => contract.service_contracts_id === 7)
 
 
-  const activityTranslations = await dataFetch("https://api.fivespark.com/items/activities_translations")
+	const activityTranslations = await dataFetch("https://api.fivespark.com/items/activities_translations")
+	// const all_contracts = [contract_basic, contract_complete, contract_premium]
 
-  console.log(activityTranslations)
-  
+	console.log(activityTranslations)
+	// const pillarCategories = await dataFetch("https://api.fivespark.com/items/pillar_categories")
+	// const activities_translations = await dataFetch("https://api.fivespark.com/items/activities_translations")
+	// const pillarCategory = pillarCategories.data.filter(data => data.id === 4)
 
-  res.render("index", { selectedContracts, contract_basic, contract_premium, contract_complete, activityTranslations,})
+
+	res.render("index", { selectedContracts, contract_basic, contract_premium, contract_complete, activityTranslations,})
 })
 
 
